7dedbf4be7fbdb973c99752449efd38282c36baa
Bartlomiej Mika <bmika@icloud.com>
2022-03-30 23:46:29 -0400

    Initial Commit

---
+++ BGTaskSample/BGTaskSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGTaskSampleApp.swift
+//  BGTaskSample
+//
+//  Created by Bartlomiej Mika on 2022-03-30.
+//
+
+import SwiftUI
+
+@main
+struct BGTaskSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BGTaskSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BGTaskSample
+//
+//  Created by Bartlomiej Mika on 2022-03-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
